60ccf942da48431d26dda3c24046c759d40f595e
Nick Fagan <nick@ifagan.com>
2019-07-28 20:15:31 -0300

    Basic functionality

--- src/blocks/music.rs
+++ src/blocks/music.rs
@@ -20,6 +20,21 @@ use crate::blocks::dbus::arg::{Array, RefArg};
 use self::stdintf::org_freedesktop_dbus::Properties;
 use uuid::Uuid;
 
+#[derive(Clone)]
+pub enum Player {
+    Mocp,
+    Dbus(String),
+    Auto,
+}
+impl Player {
+    pub fn is_auto(&self) -> bool {
+        match self {
+            Player::Auto => true,
+            _ => false,
+        }
+    }
+}
+
 pub struct Music {
     id: String,
     current_song: RotatingTextWidget,
@@ -31,7 +46,7 @@ pub struct Music {
     dbus_conn: Connection,
     player_avail: bool,
     marquee: bool,
-    player: Option<String>,
+    player: Player,
     auto_discover: bool
 }
 
@@ -171,11 +186,10 @@ impl ConfigBlock for Music {
                 .block_error("music", "failed to establish D-Bus connection")?,
             player_avail: false,
             auto_discover: block_config.player.is_none(),
-            player: if block_config.player.is_none()
-                {
-                    block_config.player
-                } else {
-                    Some(format!("org.mpris.MediaPlayer2.{}", block_config.player.unwrap()))
+            player: match block_config.player {
+                    Some(ref s) if s == "mocp" => Player::Mocp,
+                    Some(ref s) => Player::Dbus(format!("org.mpris.MediaPlayer2.{}", s)),
+                    None => Player::Auto,
                 },
             marquee: block_config.marquee,
         })
@@ -193,52 +207,103 @@ impl Block for Music {
         } else {
             (false, None)
         };
-        if !rotated && self.player.is_none() {
+        if !rotated && self.player.is_auto() {
             self.player = get_first_available_player(&self.dbus_conn)
         }
-        if !(rotated || self.player.is_none()) {
-            let c = self.dbus_conn.with_path(
-                self.player.clone().unwrap(),
-                "/org/mpris/MediaPlayer2",
-                1000,
-            );
-            let data = c.get("org.mpris.MediaPlayer2.Player", "Metadata");
+        match self.player {
+            Player::Mocp => {
+                let status = match Command::new("sh")
+                    .args(&["-c", "mocp -Q '%state' 2>/dev/null"]).output() {
+                        Ok(output) => {
+                            if output.status.success() {
+                                let mut v = output.stdout;
+                                if v.is_empty(){
+                                    "STOP".to_string()
+                                } else {
+                                    v.pop(); //remove newline
+                                    String::from_utf8(v)
+                                        .unwrap_or("STOP".to_string())
+                                }
+                            } else {
+                                "STOP".to_string()
+                            }
+                        },
+                        _ => "STOP".to_string(),
+                    };
+                match &*status {
+                    "PLAY" => {
+                        if let Some(ref mut play) = self.play {
+                            play.set_icon("music_pause");
+                        }
+                        let desc = mocp_get_song_artist().unwrap_or(String::from(""));
+                        self.player_avail = !desc.is_empty();
+                        self.current_song.set_text(desc);
+                    },
+                    "PAUSE" => {
+                        if let Some(ref mut play) = self.play {
+                            play.set_icon("music_play");
+                        }
+                        let desc = mocp_get_song_artist().unwrap_or(String::from(""));
+                        self.player_avail = !desc.is_empty();
+                        self.current_song.set_text(desc);
+                    },
+                    _ => {
+                        if let Some(ref mut play) = self.play {
+                            play.set_icon("music_play");
+                        }
+                        self.current_song.set_text(String::from(""));
+                        self.player_avail = false;
+                        if self.auto_discover {
+                            self.player = Player::Auto;
+                        }
+                    },
+                };
+            },
+            Player::Dbus(ref player) if !rotated => {
+                let c = self.dbus_conn.with_path(
+                    player.clone(),
+                    "/org/mpris/MediaPlayer2",
+                    1000,
+                );
+                let data = c.get("org.mpris.MediaPlayer2.Player", "Metadata");
 
-            if let Ok(metadata) = data {
-                let (title, artist) = extract_from_metadata(&metadata).unwrap_or((String::new(), String::new()));
+                if let Ok(metadata) = data {
+                    let (title, artist) = extract_from_metadata(&metadata).unwrap_or((String::new(), String::new()));
 
-                if title.is_empty() && artist.is_empty() {
-                    self.player_avail = false;
-                    self.current_song.set_text(String::new());
+                    if title.is_empty() && artist.is_empty() {
+                        self.player_avail = false;
+                        self.current_song.set_text(String::new());
+                    } else {
+                        self.player_avail = true;
+                        self.current_song
+                            .set_text(format!("{} | {}", title, artist));
+                    }
                 } else {
-                    self.player_avail = true;
-                    self.current_song
-                        .set_text(format!("{} | {}", title, artist));
+                    self.current_song.set_text(String::from(""));
+                    self.player_avail = false;
+                    if self.auto_discover {
+                        self.player = Player::Auto;
+                    }
                 }
-            } else {
-                self.current_song.set_text(String::from(""));
-                self.player_avail = false;
-                if self.auto_discover {
-                    self.player = None;
-                }
-            }
 
-            if let Some(ref mut play) = self.play {
-                let data = c.get("org.mpris.MediaPlayer2.Player", "PlaybackStatus");
-                match data {
-                    Err(_) => play.set_icon("music_play"),
-                    Ok(data) => {
-                        let data: Box<RefArg> = data;
-                        let state = data;
-                        if state.as_str().map(|s| s != "Playing").unwrap_or(false) {
-                            play.set_icon("music_play")
-                        } else {
-                            play.set_icon("music_pause")
+                if let Some(ref mut play) = self.play {
+                    let data = c.get("org.mpris.MediaPlayer2.Player", "PlaybackStatus");
+                    match data {
+                        Err(_) => play.set_icon("music_play"),
+                        Ok(data) => {
+                            let data: Box<RefArg> = data;
+                            let state = data;
+                            if state.as_str().map(|s| s != "Playing").unwrap_or(false) {
+                                play.set_icon("music_play")
+                            } else {
+                                play.set_icon("music_pause")
+                            }
                         }
                     }
                 }
-            }
-        }
+            },
+            _ => {}
+        };
         Ok(match (next, self.marquee) {
             (Some(_), _) => next,
             (None, _) => Some(Duration::new(2, 0))
@@ -247,33 +312,58 @@ impl Block for Music {
 
     fn click(&mut self, event: &I3BarEvent) -> Result<()> {
         if let Some(ref name) = event.name {
-            let action = match name as &str {
-                "play" => "PlayPause",
-                "next" => "Next",
-                "prev" => "Previous",
-                _ => "",
-            };
-            if action != "" {
-                let m = Message::new_method_call(
-                    self.player.as_ref().unwrap(),
-                    "/org/mpris/MediaPlayer2",
-                    "org.mpris.MediaPlayer2.Player",
-                    action,
-                ).block_error("music", "failed to create D-Bus method call")?;
-                self.dbus_conn
-                    .send(m)
-                    .block_error("music", "failed to call method via D-Bus")
-                    .map(|_| ())
-            } else {
-                if name == "on_collapsed_click" && self.on_collapsed_click.is_some() {
-                    let command = self.on_collapsed_click.clone().unwrap();
-                    let command_broken: Vec<&str> = command.split_whitespace().collect();
-                    let mut itr = command_broken.iter();
-                    let mut _cmd = Command::new(OsStr::new(&itr.next().unwrap()))
-                        .args(itr)
-                        .spawn();
+            match self.player {
+                Player::Mocp => {
+                    let action = match name as &str {
+                        "play" => "--toggle-pause",
+                        "next" => "--next",
+                        "prev" => "--previous",
+                        _ => "",
+                    };
+                    if action != "" {
+                        if !Command::new("sh")
+                            .args(&["-c", &format!("mocp {}", action)])
+                            .status()
+                            .block_error("music", "could not do action")
+                            .map(|s| s.success())? {
+                            return Err(BlockError("music".to_string(),"bad status when doing action".to_string()));
+                        } else {
+                            self.update()?;
+                        }
+                    }
+                    Ok(())
                 }
-                Ok(())
+                Player::Dbus(ref pname) => {
+                    let action = match name as &str {
+                        "play" => "PlayPause",
+                        "next" => "Next",
+                        "prev" => "Previous",
+                        _ => "",
+                    };
+                    if action != "" {
+                        let m = Message::new_method_call(
+                            pname,
+                            "/org/mpris/MediaPlayer2",
+                            "org.mpris.MediaPlayer2.Player",
+                            action,
+                        ).block_error("music", "failed to create D-Bus method call")?;
+                        self.dbus_conn
+                            .send(m)
+                            .block_error("music", "failed to call method via D-Bus")
+                            .map(|_| ())
+                    } else {
+                        if name == "on_collapsed_click" && self.on_collapsed_click.is_some() {
+                            let command = self.on_collapsed_click.clone().unwrap();
+                            let command_broken: Vec<&str> = command.split_whitespace().collect();
+                            let mut itr = command_broken.iter();
+                            let mut _cmd = Command::new(OsStr::new(&itr.next().unwrap()))
+                                .args(itr)
+                                .spawn();
+                        }
+                        Ok(())
+                    }
+                },
+                Player::Auto => Ok(()),
             }
         } else {
             Ok(())
@@ -288,10 +378,10 @@ impl Block for Music {
                 elements.push(prev);
             }
             if let Some(ref play) = self.play {
-                elements.push(play);;
+                elements.push(play);
             }
             if let Some(ref next) = self.next {
-                elements.push(next);;
+                elements.push(next);
             }
             elements
         } else {
@@ -302,6 +392,23 @@ impl Block for Music {
             }
         }
     }
+}
+
+fn mocp_get_song_artist() -> Result<String> {
+    Command::new("sh")
+        .args(&["-c", "mocp -Q '%song | %artist' 2>/dev/null"])
+        .output()
+        .block_error("music", "failed to extract metadata")
+        .map(|output| {
+            let mut v = output.stdout;
+            if v.is_empty() {
+                String::from("")
+            } else {
+                v.pop(); // remove newline
+                String::from_utf8(v)
+                    .unwrap_or(String::from(""))
+            }
+        })
 }
 
 fn extract_from_metadata(metadata: &Box<arg::RefArg>) -> Result<(String, String)> {
@@ -346,14 +453,20 @@ fn extract_from_metadata(metadata: &Box<arg::RefArg>) -> Result<(String, String)
     Ok((title, artist))
 }
 
-fn get_first_available_player(connection: &Connection) -> Option<String> {
+fn get_first_available_player(connection: &Connection) -> Player {
     let m = Message::new_method_call("org.freedesktop.DBus", "/", "org.freedesktop.DBus", "ListNames").unwrap();
     let r = connection.send_with_reply_and_block(m, 2000).unwrap();
     // ListNames returns one argument, which is an array of strings.
     let mut arr: Array<&str, _>  = r.get1().unwrap();
     if let Some(name) = arr.find(|entry| entry.starts_with("org.mpris.MediaPlayer2")) {
-        Some(String::from(name))
+        Player::Dbus(String::from(name))
+    } else if Command::new("sh")
+        .args(&["-c","mocp -i >/dev/null 2>&1"])
+        .status()
+        .map(|s| s.success())
+        .unwrap_or(false) {
+            Player::Mocp
     } else {
-        None
+        Player::Auto
     }
 }
